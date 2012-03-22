800ff1022861c3b2e1439427275bd11b71d2b372
amoslanka <amoslanka@gmail.com>
2012-03-22 09:58:49 -0700

    work on amon

--- amon/metadata.rb
+++ amon/metadata.rb
@@ -3,12 +3,11 @@ maintainer_email  "nerds@struck.com"
 description       "Installs amon and amond"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.0.0"
-recipe            "build-essential", "Installs C compiler and build tools on Linux"
 
-depends           "build-essential"
-depends           "mongodb" # Mongo db for debian boxes
-depends           "python"
-depends           "tornado"
+# depends           "build-essential"
+# depends           "mongodb" # Mongo db for debian boxes
+# depends           "python"
+# depends           "tornado"
 
 # www.amon.cx
 # More installation instruction at http://amon.cx/guide/install/
--- amon/recipes/default.rb
+++ amon/recipes/default.rb
@@ -5,30 +5,49 @@
 # Copyright 2012 Struck
 #
 
-require_recipe 'build-essential'
-require_recipe 'mongodb'
-require_recipe 'tornado'
+# require_recipe 'build-essential'
+# require_recipe 'mongodb'
+# require_recipe 'tornado'
 
-packages = ['curl']
-rpm_linux = platform?("redhat", "centos", "fedora")
-deb_linux = platform?("ubuntu", "debian")
-
-packages += ["gcc", "python-dev", "sysstat"] if deb_linux
-packages += ["gcc", "python-devel", "sysstat"] if rpm_linux
-
-packages.each do |pkg|
-  package pkg
+execute "curl install.amon.cx | bash" do
+  cwd Chef::Config[:file_cache_path]
+  user 'root'
+  group 'root'
 end
 
-# Creates the user that will own the amon files and run the services.
-user node[:amon][:user] do
-  shell "/bin/bash"
-  action :create
-end
+# remote_file File.join(Chef::Config[:file_cache_path], "install.amon.cx") do 
+#   owner 'root'
+#   source "http://install.amon.cx"
+# end
 
-# Apply the config settings based on node[:amon][:config]
-# require_recipe 'amon::config'
-# Install amon using the specified method. 
-log("Invalid install method"){ level :fatal } unless [:auto, :manual].include?(node[:amon][:install_method])
-include_recipe "amon::#{node[:amon][:install_method]}"
+# execute "./install.amon.cx" do 
+#   cwd Chef::Config[:file_cache_path]
+#   user 'root'
+#   group 'root'
+# end
 
+
+
+# packages = ['curl']
+# rpm_linux = platform?("redhat", "centos", "fedora")
+# deb_linux = platform?("ubuntu", "debian")
+# 
+# packages += ["gcc", "python-dev", "sysstat"] if deb_linux
+# packages += ["gcc", "python-devel", "sysstat"] if rpm_linux
+# 
+# packages.each do |pkg|
+#   package pkg
+# end
+# 
+# # Creates the user that will own the amon files and run the services.
+# user node[:amon][:user] do
+#   shell "/bin/bash"
+#   action :create
+# end
+# 
+# # Apply the config settings based on node[:amon][:config]
+# # require_recipe 'amon::config'
+# # Install amon using the specified method. 
+# log("Invalid install method"){ level :fatal } unless [:auto, :manual].include?(node[:amon][:install_method])
+# include_recipe "amon::#{node[:amon][:install_method]}"
+# 
